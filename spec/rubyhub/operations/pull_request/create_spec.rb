cc638318ac61cd4773c3c55a8f008d5a8885e065
DELL <nswells14@gmail.com>
2020-03-27 14:31:09 +0200

    Added PullRequest create tests

--- spec/rubyhub/operations/pull_request/create_spec.rb
+++ spec/rubyhub/operations/pull_request/create_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe Rubyhub::Operations::PullRequest::Create do
   let(:options) { { 'template': 'backend' } }
+  let(:data) { { labels: 'label' } }
 
   describe '.call' do
     context 'when configuration does not exist' do
@@ -23,6 +24,18 @@ RSpec.describe Rubyhub::Operations::PullRequest::Create do
         expect { described_class.call(options) }.to raise_error(Rubyhub::IncorrectTemplateError) do |ex|
           expect(ex.message).to eq('You haven\'t pass the template name or template name not present in config file')
         end
+      end
+    end
+
+    context 'when template name is provided' do
+      before do
+        allow(Rubyhub::Configuration).to receive(:exists?).and_return(true, true)
+        allow(described_class).to receive(:data).and_return(data)
+      end
+
+      it 'passes data to Rubyhub::PullRequest ' do
+        expect(described_class).to receive(:call).with(options).and_call_original
+        described_class.call(options)
       end
     end
   end
